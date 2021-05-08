e698749ad11ff4deecdbd4cd8b467ac4338fa2a9
Dongjin Baek <dj9136@gmail.com>
2021-05-09 03:05:15 +0900

    changed makefile for CI

--- run-filechecks.sh
+++ run-filechecks.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-
 if [ "$#" -ne 1 ]; then
   echo "run-filecheck.sh <FileCheck path>"
   exit 1
 fi
-
 echo "--- Start FileCheck.. ---"
 set -e
 
-for i in `find ./filechecks -name "*.ll"` ; do
-  ./sf-compiler $i -o .tmp.s
-  $1 $i < .tmp.s
+PASSED=0
+TOTAL=0
+for f in `ls -1 filechecks` ; do
+  echo "== filechecks/${f} =="
+  ./bin/sf-compiler filechecks/${f} filechecks/${f}.s > filechecks/tmp.s
+  $1 filechecks/${f} < filechecks/${f}.s
+  if [ "$?" -eq 0 ]; then
+    PASSED=$((PASSED+1))
+  fi
+  TOTAL=$((TOTAL+1))
+  rm ./filechecks/${f}.s
 done
+rm filechecks/*.s
+echo "Test passed: $PASSED / $TOTAL"
