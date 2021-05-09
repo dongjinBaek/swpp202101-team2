d3640d5b31fa9b41b5207d1345744bdd1d350cca
Dongjin Baek <dj9136@gmail.com>
2021-05-09 11:46:31 +0900

    Add benchmark tests to CI

--- run-filechecks.sh
+++ run-filechecks.sh
@@ -8,15 +8,15 @@ set -e
 
 PASSED=0
 TOTAL=0
+mkdir -p filechecks-out
 for f in `ls -1 filechecks` ; do
   echo "== filechecks/${f} =="
-  ./bin/sf-compiler filechecks/${f} filechecks/${f}.s > filechecks/tmp.s
-  $1 filechecks/${f} < filechecks/${f}.s
+  ./bin/sf-compiler filechecks/${f} filechecks-out/${f}.s > filechecks-out/tmp.s
+  $1 filechecks/${f} < filechecks-out/${f}.s
   if [ "$?" -eq 0 ]; then
     PASSED=$((PASSED+1))
   fi
   TOTAL=$((TOTAL+1))
-  rm ./filechecks/${f}.s
 done
-rm filechecks/*.s
+rm -r filechecks-out
 echo "Test passed: $PASSED / $TOTAL"
