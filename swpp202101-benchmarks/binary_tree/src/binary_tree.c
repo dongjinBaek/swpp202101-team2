0568468ac0dc0b6283c41f6234a1ecce64043b1f
zenith82114 <43979831+zenith82114@users.noreply.github.com>
2021-05-29 21:07:53 +0900

    [Sprint 3, NFC] 5/28 benchmark patch (#24)
    * benchmark analysis improved
    
    * bugfix
    
    * [Sprint 1, NFC] Interpreter (revised) and benchmarks (#1)
    
    * benchmark analysis improved
    
    * bugfix
    
    * makefile and script modified
    
    * main patch applied
    
    * Implement basic logic
    
    * Consider recursive calls
    
    * Add condbranchdeflation.ll
    
    * Bugfix
    
    * Improved comments
    
    * Change filechecks
    
    * Delete old filecheck
    
    * IROutlinerPass: find split point
    
    * Add IROutliner filecheck
    
    * Fix filechecks
    
    * bugfix
    
    * Reflect PR suggestions
    
    * Copy and paste updated benchmarks
    
    Co-authored-by: baneling100 <whnbaek@gmail.com>

--- swpp202101-benchmarks/binary_tree/src/binary_tree.c
+++ swpp202101-benchmarks/binary_tree/src/binary_tree.c
@@ -4,7 +4,18 @@
 uint64_t read();
 void write(uint64_t val);
 
-uint64_t insert(uint64_t* root, uint64_t data) {
+extern uint64_t* root;
+
+uint64_t insert(uint64_t data) {
+  if (root == NULL) {
+    uint64_t* node = (uint64_t*)malloc(3 * sizeof(uint64_t));
+    *node = data;
+    *(node + 1) = (uint64_t)NULL;
+    *(node + 2) = (uint64_t)NULL;
+    root = node;
+    return 1;
+  }
+
   uint64_t* curr = root;
 
   while (1) {
@@ -41,62 +52,85 @@ uint64_t insert(uint64_t* root, uint64_t data) {
   }
 }
 
-uint64_t* adjust(uint64_t* node) {
-  uint64_t* rightmost = (uint64_t*)*(node + 1);
-  if (rightmost == NULL)
-    return (uint64_t*)*(node + 2);
-
-  uint64_t* parent = NULL;
-
-  while ((uint64_t*)*(rightmost + 2) != NULL) {
-    parent = rightmost;
-    rightmost = (uint64_t*)*(rightmost + 2);
-  }
-
-  if (parent != NULL)
-    *(parent + 2) = *(rightmost + 1);
-
-  return rightmost;
-}
-
-uint64_t remove(uint64_t* root, uint64_t data) {
-  uint64_t* curr = root;
-
-  if (*curr == data)
-    return 0;
+uint64_t adjust(uint64_t* node) {
+  uint64_t* parent = node;
+  uint64_t* curr = (uint64_t*)*(node + 1);
 
   while (1) {
+    uint64_t data = *curr;
+    uint64_t* left = (uint64_t*)*(curr + 1);
+    uint64_t* right = (uint64_t*)*(curr + 2);
+
+    if (right == NULL) {
+      if (curr == (uint64_t*)*(parent + 1))
+        *(parent + 1) = (uint64_t)left;
+      else
+        *(parent + 2) = (uint64_t)right;
+     
+      free(curr);
+      return data;
+    }
+
+    parent = curr;
+    curr = right;
+  }
+}
+
+uint64_t remove(uint64_t data) {
+  if (root == NULL)
+    return 0;
+
+  uint64_t* parent = NULL;
+  uint64_t* curr = root;
+
+  while (1) {
+    if (curr == NULL)
+      return 0;
+
     uint64_t curr_data = *curr;
-    uint64_t* next;
+    uint64_t* left = (uint64_t*)*(curr + 1);
+    uint64_t* right = (uint64_t*)*(curr + 2);
 
-    if (curr_data > data) {
-      next = (uint64_t*)*(curr + 1);
-      if (next == NULL)
-        return 0;
-
-      if (*next == data) {
-        *(curr + 1) = (uint64_t)adjust(next);
-        free(next);
-        return 1;
-      }
-
-      curr = next;
+    if (data < curr_data) {
+      parent = curr;
+      curr = (uint64_t*)*(curr + 1);
       continue;
     }
-    else if (curr_data < data) {
-      next = (uint64_t*)*(curr + 2);
-      if (next == NULL)
-        return 0;
 
-      if (*next == data) {
-        *(curr + 2) = (uint64_t)adjust(next);
-        free(next);
-        return 1;
-      }
-
-      curr = next;
+    if (data > curr_data) {
+      parent = curr;
+      curr = (uint64_t*)*(curr + 2);
       continue;
     }
+
+    if (left == NULL) {
+      if (parent == NULL)
+        root = right;
+      else if (curr == (uint64_t*)*(parent + 1))
+        *(parent + 1) = (uint64_t)right;
+      else
+        *(parent + 2) = (uint64_t)right;
+
+      free(curr);
+      return 1;
+    }
+
+    if (right == NULL) {
+      if (parent == NULL)
+        root = left;
+      else if (curr == (uint64_t*)*(parent + 1))
+        *(parent + 1) = (uint64_t)left;
+      else
+        *(parent + 2) = (uint64_t)left;
+
+      free(curr);
+      return 1;
+    }
+
+    uint64_t new_data = adjust(curr);
+    *curr = new_data;
+
+    return 1;
   }
 }
 
@@ -115,24 +149,18 @@ void traverse(uint64_t* node) {
 }
 
 int main() {
-  uint64_t* head = (uint64_t*)malloc(3 * sizeof(uint64_t));
-  uint64_t init = read();
-
-  *head = init;
-  *(head + 1) = (uint64_t)NULL;
-  *(head + 2) = (uint64_t)NULL;
-
+  root = NULL;
+  
   uint64_t n = read();
-
   for (uint64_t i = 0; i < n; i++) {
     uint64_t action = read();
     uint64_t data = read();
 
     if (action == 0)
-      insert(head, data);
+      insert(data);
     else
-      remove(head, data);
+      remove(data);
   }
 
-  traverse(head);
+  traverse(root);
 }
