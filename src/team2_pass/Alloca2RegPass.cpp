9a6c9604548f9127d12e84f5a54292ab28f2133a
Dongjin Baek <dj9136@gmail.com>
2021-06-06 02:38:46 +0900

    change store inst to switch

--- src/team2_pass/Alloca2RegPass.cpp
+++ src/team2_pass/Alloca2RegPass.cpp
@@ -11,7 +11,6 @@ using namespace llvm::PatternMatch;
 namespace team2_pass {
 
 PreservedAnalyses Alloca2RegPass::run(Module &M, ModuleAnalysisManager &MAM) {
-            // Store = M.getOrInsertFunction("$store", Type::getVoidTy(M.getContext()), ET, ET);
             Regs = vector<LoadInst *>();
   for (auto &F : M) {
     int cnt = 0;
@@ -22,6 +21,7 @@ PreservedAnalyses Alloca2RegPass::run(Module &M, ModuleAnalysisManager &MAM) {
           Type *AT = AI->getAllocatedType();
           if (AT->isArrayTy() && (n = AT->getArrayNumElements()) <= 10) {
             Type *ET = AT->getArrayElementType();
+            Store = M.getOrInsertFunction("$store", Type::getVoidTy(M.getContext()), ET, ET);
             Regs.clear();
             for (int i=0; i<n; i++) {
               Constant *Reg = M.getOrInsertGlobal("$reg"+to_string(i), ET);
@@ -80,31 +80,29 @@ PreservedAnalyses Alloca2RegPass::run(Module &M, ModuleAnalysisManager &MAM) {
           LI->replaceAllUsesWith(Phi);
           // LI->eraseFromParent();
         } else if (StoreInst* SI = dyn_cast<StoreInst>(I)) {
-          // // split current basic block in two, diving from LI
-          // outs() << "0\n";
-          // auto *DownBB = SI->getParent();
-          // auto *UpBB = DownBB->splitBasicBlockBefore(LI, "splitted");
-          // outs() << *UpBB << "\n";
-          // // insert n simple basic blocks for switch
-          // vector<BasicBlock *> SwitchBBs;
-          // for (int i=0; i<n; i++) {
-          //   auto *SwitchBB = BasicBlock::Create(F.getContext(), "splitted_bb"+to_string(i), &F, DownBB);
-          //   IRBuilder<> builder(SwitchBB);
-          //   auto *Br = BranchInst::Create(DownBB);
-          //   builder.Insert(Br);
-          //   // Value *Args[] = {Regs[i], SI->getOperand(0)};
-          //   // CallInst *StoreCI = CallInst::Create(Store, ArrayRef<Value *>(Args, 2));
-          //   // builder.Insert(StoreCI);
-          //   SwitchBBs.push_back(SwitchBB);
-          // }
-          // outs() << "1\n";
-          // // change terminator inst of upper splitted block to switch
-          // UpBB->getTerminator()->eraseFromParent();
-          // SwitchInst *SwI = SwitchInst::Create(GEPI->getOperand(2), SwitchBBs[0], n, UpBB);
-          // for (int i=0; i<n; i++) {
-          //   ConstantInt *CI = ConstantInt::get(dyn_cast<IntegerType>(GEPI->getOperand(2)->getType()), i);
-          //   SwI->addCase(CI, SwitchBBs[i]);
-          // }
+          // split current basic block in two, diving from LI
+          auto *DownBB = SI->getParent();
+          auto *UpBB = DownBB->splitBasicBlockBefore(SI, "splitted");
+          // insert n simple basic blocks for switch
+          vector<BasicBlock *> SwitchBBs;
+          for (int i=0; i<n; i++) {
+            auto *SwitchBB = BasicBlock::Create(F.getContext(), "splitted_bb"+to_string(i), &F, DownBB);
+            IRBuilder<> builder(SwitchBB);
+            auto *Br = BranchInst::Create(DownBB);
+            builder.Insert(Br);
+            Value *Args[] = {Regs[i], SI->getOperand(0)};
+            CallInst *StoreCI = CallInst::Create(Store, ArrayRef<Value *>(Args, 2), "", Br);
+            SwitchBBs.push_back(SwitchBB);
+          }
+          // change terminator inst of upper splitted block to switch
+          UpBB->getTerminator()->eraseFromParent();
+          SwitchInst *SwI = SwitchInst::Create(GEPI->getOperand(2), SwitchBBs[0], n, UpBB);
+          for (int i=0; i<n; i++) {
+            ConstantInt *CI = ConstantInt::get(dyn_cast<IntegerType>(GEPI->getOperand(2)->getType()), i);
+            SwI->addCase(CI, SwitchBBs[i]);
+          }
+        } else if (GetElementPtrInst *GI = dyn_cast<GetElementPtrInst>(I)) {
+          changeUseOfGEPToSwitch(GI, F);
         }
       }
     }
